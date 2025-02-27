7b56c7c6bbdfcddd7de38c133bdf97ccc33c9641
ebnybuniad <ebnybuniad@hmail.com>
2025-02-27 17:52:29 +0600

    added wrist size

--- js/script.js
+++ js/script.js
@@ -16,3 +16,16 @@ for (const ringButton of ringButtons) {
         productImage.src = "../images/" + colorName + ".png";
     });
 }
+
+function selectWristSize(size) {
+    const sizes = ["S", "M", "L", "XL"];
+    for (let i = 0; i < sizes.length; i++) {
+        const button = document.getElementById("size-" + sizes[i]);
+        const element = sizes[i];
+        if (size === element) {
+            button.classList.add("border-purple-600");
+        }else{
+            button.classList.remove("border-purple-600");
+        }
+    }
+}
